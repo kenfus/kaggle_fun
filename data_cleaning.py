ea910a55fb0c5d44fecbd7c24d3d5b186d623114
vtimmel <vtimmel@ethz.ch>
2019-06-09 16:19:04 +0200

    score 0.824ls

---
+++ data_cleaning.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 15:54:39 2019
+
+@author: Vincenzo
+"""
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import pandas as pd
+
+def clean_fe(df, drop_outliners = False):
+    df_dropped = df.drop(['PassengerId','Cabin','Ticket'], axis = 1)
+    df_dropped['Fare']=df_dropped['Fare'].fillna(df_dropped['Fare'].median())
+    df_dropped['Embarked']=df_dropped['Embarked'].fillna('S')
+    
+    dataset_title = [i.split(",")[1].split(".")[0].strip() for i in df_dropped["Name"]]
+    df_dropped["Title"] = pd.Series(dataset_title)
+    df_dropped["Title"] = df_dropped["Title"].replace(['Lady', 'the Countess','Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    df_dropped["Title"] = df_dropped["Title"].map({"Master":0, "Miss":1, "Ms" : 1 , "Mme":1, "Mlle":1, "Mrs":1, "Mr":2, "Rare":3})
+    df_dropped["Title"] = df_dropped["Title"].astype(int)
+
+    df_dropped['Age']=np.where((df_dropped['Title']=='Master') & (np.isnan(df_dropped['Age'])), 12, df_dropped['Age'])
+    df_dropped['Age']=np.where((df_dropped['Title']=='Miss') & (np.isnan(df_dropped['Age'])), 14, df_dropped['Age'])
+    df_dropped['Age']=np.where((df_dropped['Title']=='Mr') & (np.isnan(df_dropped['Age'])), 35, df_dropped['Age'])
+    df_dropped['Age']=np.where((df_dropped['Title']=='Mrs') & (np.isnan(df_dropped['Age'])), 24, df_dropped['Age'])
+    df_dropped['Age']=df_dropped['Age'].fillna(df_dropped['Age'].median())
+    
+    df_dropped['AgeStatus'] = np.nan
+    df_dropped['AgeStatus'] = np.where((df_dropped['Age']<=12),0,df_dropped['AgeStatus'])
+    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>12) & (df_dropped['Age']<=18),1,df_dropped['AgeStatus'] ) 
+    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>18) & (df_dropped['Age']<=24),2,df_dropped['AgeStatus'] )
+    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>24) & (df_dropped['Age']<=55),3,df_dropped['AgeStatus'] ) 
+    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>55) & (df_dropped['Age']<=70),4,df_dropped['AgeStatus'] ) 
+    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>70),5,df_dropped['AgeStatus'] ) 
+
+
+    df_dropped = df_dropped.drop(['Name','Age'], axis = 1)
+    df_drop_nonan = df_dropped.dropna()
+    
+    df_drop_nonan.loc[:,'IsAlone'] = np.where(df_drop_nonan.loc[:,'Parch'] + df_drop_nonan.loc[:,'SibSp'] == 0,1,0)
+
+    
+    df_drop_nonan = df_drop_nonan.replace('male',1)
+    df_drop_nonan = df_drop_nonan.replace('female',0)
+    
+    df_drop_nonan['Embarked'] = df_drop_nonan['Embarked'].replace('S',0)
+    df_drop_nonan['Embarked'] = df_drop_nonan['Embarked'].replace('C',1)
+    df_drop_nonan['Embarked'] = df_drop_nonan['Embarked'].replace('Q',2)
+    
+    from scipy import stats
+    if drop_outliners == True:
+        df_drop_nonan = df_drop_nonan[(np.abs(stats.zscore(df_drop_nonan)) < 3).all(axis=1)]
+    
+    df_drop_nonan.reset_index(inplace = True)
+     
+    scaler = StandardScaler()
+    columns_to_scale = ['Fare']
+
+    temp_df = df_drop_nonan[columns_to_scale]
+
+    temp_df = pd.DataFrame(scaler.fit_transform(temp_df))
+    temp_df.columns = columns_to_scale
+    
+    df_drop_nonan_sc = df_drop_nonan.drop(columns_to_scale, axis = 1)
+    df_drop_nonan_sc = df_drop_nonan_sc.join(temp_df)
+    
+    return df_drop_nonan_sc
--- main.py
+++ main.py
@@ -6,3 +6,173 @@ Created on Thu Jun  6 15:38:11 2019
 @author: vtimmel
 """
 
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+import numpy as np
+
+from data_cleaning import clean_fe
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+test_cleaned = clean_fe(train)
+
+X_train, X_test, y_train, y_test = train_test_split(test_cleaned.loc[:,'Pclass':].values,
+                                                    test_cleaned.loc[:,'Survived'].values,
+                                                    test_size=0.2)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knc = KNeighborsClassifier(n_jobs = 1)
+
+parameters = {'n_neighbors': np.arange(2,6)}
+
+knc = GridSearchCV(knc, parameters, cv=5)
+
+knc.fit(X_train, y_train)
+
+knc_best = knc.best_estimator_
+
+print('knc: ',knc.best_params_, knc.best_score_)
+
+
+from sklearn.ensemble import RandomForestClassifier
+
+#create a new random forest classifier
+rf = RandomForestClassifier()
+
+#create a dictionary of all values we want to test for n_estimators
+params_rf = {'n_estimators': np.arange(20, 30) }
+
+#use gridsearch to test all values for n_estimators
+rf_gs = GridSearchCV(rf, params_rf, cv=5)
+
+#fit model to training data
+rf_gs.fit(X_train, y_train)
+
+#save best model
+rf_best = rf_gs.best_estimator_
+
+#check best n_estimators value
+print('rfc: ',rf_gs.best_params_, rf_gs.best_score_)
+
+
+from sklearn.svm import SVC
+
+#create a new random forest classifier
+svc = SVC(gamma = 'scale', probability=True)
+
+#create a dictionary of all values we want to test for n_estimators
+params_svc = {'C' : np.arange(1,4)}
+
+#use gridsearch to test all values for n_estimators
+svc_gs = GridSearchCV(svc, params_svc, cv=5)
+
+#fit model to training data
+svc_gs.fit(X_train, y_train)
+
+#save best model
+svc_best = svc_gs.best_estimator_
+
+#check best n_estimators value
+print('svc: ',svc_gs.best_params_, svc_gs.best_score_)
+
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+#create a new random forest classifier
+gbc = GradientBoostingClassifier()
+
+#create a dictionary of all values we want to test for n_estimators
+params_gbc = {'n_estimators': np.arange(25,45,1),'learning_rate': np.arange(0.01,0.4,0.01) }
+
+#use gridsearch to test all values for n_estimators
+gbc_gs = GridSearchCV(gbc, params_gbc, cv=5)
+
+#fit model to training data
+gbc_gs.fit(X_train, y_train)
+
+#save best model
+gbc_best = gbc_gs.best_estimator_
+
+#check best n_estimators value
+print('gbc: ',gbc_gs.best_params_, gbc_gs.best_score_)
+
+
+import xgboost as xgb
+
+params_xgb = {'max_depth': np.arange(3,7)}
+
+xgb_model = xgb.XGBClassifier()
+
+xgb_gs = GridSearchCV(xgb_model, params_xgb, cv=5)
+
+xgb_gs.fit(X_train, y_train)
+
+xgb_best = xgb_gs.best_estimator_
+
+print('xgb: ',xgb_gs.best_params_, xgb_gs.best_score_)
+
+
+from sklearn.ensemble import AdaBoostClassifier
+
+#create a new random forest classifier
+adc = AdaBoostClassifier()
+
+#create a dictionary of all values we want to test for n_estimators
+params_adc = {'n_estimators': np.arange(22,34,2), 'learning_rate': np.arange(0.01,1.0,0.051) }
+
+#use gridsearch to test all values for n_estimators
+adc_gs = GridSearchCV(adc, params_adc, cv=5)
+
+#fit model to training data
+adc_gs.fit(X_train, y_train)
+
+#save best model
+adc_best = adc_gs.best_estimator_
+
+#check best n_estimators value
+print('adc: ',adc_gs.best_params_, adc_gs.best_score_)
+
+
+from sklearn.linear_model import LogisticRegression
+
+#create a new logistic regression model
+log_reg = LogisticRegression(solver = 'saga',tol = 1e-6, max_iter = 400, penalty = 'elasticnet', l1_ratio = 0.5)
+
+#fit the model to the training data
+log_reg.fit(X_train, y_train)
+
+print('log_reg', log_reg.score(X_train,y_train))
+
+
+
+from sklearn.ensemble import VotingClassifier
+
+#create a dictionary of our models
+estimators=[('knc',knc_best), ('rf', rf_best), ('log_reg', log_reg), 
+            ('gbc', gbc_best), ('SVC',svc_best),('ADC',adc_best),('xgb',xgb_best)]
+#create our voting classifier, inputting our models
+ensemble = VotingClassifier(estimators, voting='soft')
+
+#fit model to training data
+ensemble.fit(X_train, y_train)
+
+#test our model on the test data
+print(ensemble.score(X_test, y_test))
+
+
+
+#Create test data. Make a function next time, wtf.
+
+test_cleaned = clean_fe(test, False)
+
+submission = pd.DataFrame()
+prediction = pd.DataFrame(ensemble.predict(test_cleaned.loc[:,'Pclass':].values))
+
+submission['Survived']  = prediction[0]
+submission['PassengerId']=test['PassengerId']
+submission.to_csv('submission.csv',index = False)
