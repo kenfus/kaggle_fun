191e31ca9e8c4362005313104befdf6349f0b05d
vtimmel <vtimmel@ethz.ch>
2019-06-18 23:06:07 +0200

    Finally done and happy

--- titanic/data_cleaning.py
+++ titanic/data_cleaning.py
@@ -7,64 +7,120 @@ Created on Fri Jun  7 15:54:39 2019
 It's a python script to clean data and add feature engineering without copy paste it each time I changed something 
 for test, train data.
 """
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 import numpy as np
 import pandas as pd
 
+
 def clean_fe(df, drop_outliners = False):
-    df_dropped = df.drop(['PassengerId','Cabin','Ticket'], axis = 1)
-    df_dropped['Fare']=df_dropped['Fare'].fillna(df_dropped['Fare'].median())
+    df_dropped = df.drop(['PassengerId','Ticket'], axis = 1)
     df_dropped['Embarked']=df_dropped['Embarked'].fillna('S')
     
-    dataset_title = [i.split(",")[1].split(".")[0].strip() for i in df_dropped["Name"]]
-    df_dropped["Title"] = pd.Series(dataset_title)
-    df_dropped["Title"] = df_dropped["Title"].replace(['Lady', 'the Countess','Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
-    df_dropped["Title"] = df_dropped["Title"].map({"Master":0, "Miss":1, "Ms" : 1 , "Mme":1, "Mlle":1, "Mrs":1, "Mr":2, "Rare":3})
-    df_dropped["Title"] = df_dropped["Title"].astype(int)
+    dataset_title = [i.split(',')[1].split('.')[0].strip() for i in df_dropped['Name']]
 
-    df_dropped['Age']=np.where((df_dropped['Title']=='Master') & (np.isnan(df_dropped['Age'])), 12, df_dropped['Age'])
-    df_dropped['Age']=np.where((df_dropped['Title']=='Miss') & (np.isnan(df_dropped['Age'])), 14, df_dropped['Age'])
-    df_dropped['Age']=np.where((df_dropped['Title']=='Mr') & (np.isnan(df_dropped['Age'])), 35, df_dropped['Age'])
-    df_dropped['Age']=np.where((df_dropped['Title']=='Mrs') & (np.isnan(df_dropped['Age'])), 24, df_dropped['Age'])
+    df_dropped['Title'] = pd.Series(dataset_title)
+    to_fix = {'Ms':'Miss','Mme':'Madame', 'Mlle':'Mademoiselle', 'Mrs':'Missus',
+              'Mr':'Mister'}
+    df_dropped['Title'] = df_dropped['Title'].replace(to_fix)
+    df_dropped['Title'] = df_dropped['Title'].replace(['Lady', 'the Countess','Countess','Capt', 'Col','Don', 'Dr', 'Major',
+              'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    
+    df_dropped['Cabin'] = df_dropped['Cabin'].fillna('U')
+    df_dropped['Cabin'] = [i.split(' ')[0] for i in df_dropped['Cabin']]
+    df_dropped['Cabin'] = df_dropped['Cabin'].str.replace('\d', '')
+    
+    df_dropped['CabinA'] = np.where((df_dropped['Cabin']=='A'), 1,0)
+    df_dropped['CabinB'] = np.where((df_dropped['Cabin']=='B'), 1,0)
+    df_dropped['CabinC'] = np.where((df_dropped['Cabin']=='C'), 1,0)
+    df_dropped['CabinD'] = np.where((df_dropped['Cabin']=='D'), 1,0)
+    df_dropped['CabinE'] = np.where((df_dropped['Cabin']=='E'), 1,0)
+    df_dropped['CabinF'] = np.where((df_dropped['Cabin']=='F'), 1,0)
+    df_dropped['CabinG'] = np.where((df_dropped['Cabin']=='G'), 1,0)
+    df_dropped['CabinU'] = np.where((df_dropped['Cabin']=='U'), 1,0)
+    
+    df_dropped = df_dropped.drop(columns = 'Cabin')
+    
+    
+    mean_age_master = df_dropped['Age'][df_dropped['Title']=='Master'].median()
+    mean_age_mister = df_dropped['Age'][df_dropped['Title']=='Mister'].median()
+    mean_age_miss = df_dropped['Age'][df_dropped['Title']=='Miss'].median()
+    mean_age_madame = df_dropped['Age'][df_dropped['Title']=='Madame'].median()
+    mean_age_missus = df_dropped['Age'][df_dropped['Title']=='Missus'].median()
+    mean_age_madmo = df_dropped['Age'][df_dropped['Title']=='Mademoiselle'].median()
+
+
+    df_dropped['Age']=np.where((df_dropped['Title']=='Master') & (np.isnan(df_dropped['Age'])), mean_age_master, df_dropped['Age'])
+    df_dropped['Age']=np.where((df_dropped['Title']=='Mister') & (np.isnan(df_dropped['Age'])), mean_age_mister, df_dropped['Age'])
+    df_dropped['Age']=np.where((df_dropped['Title']=='Miss') & (np.isnan(df_dropped['Age'])), mean_age_miss, df_dropped['Age'])
+    df_dropped['Age']=np.where((df_dropped['Title']=='Missus') & (np.isnan(df_dropped['Age'])), mean_age_missus, df_dropped['Age'])
+    df_dropped['Age']=np.where((df_dropped['Title']=='Mademoiselle') & (np.isnan(df_dropped['Age'])), mean_age_madmo, df_dropped['Age'])
+    df_dropped['Age']=np.where((df_dropped['Title']=='Madame') & (np.isnan(df_dropped['Age'])), mean_age_madame, df_dropped['Age'])
     df_dropped['Age']=df_dropped['Age'].fillna(df_dropped['Age'].median())
+#    
+#    
+#    df_dropped['AgeStatus'] = np.nan
+#    df_dropped['AgeStatus'] = np.where((df_dropped['Age']<=8),0,df_dropped['AgeStatus'])
+#    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>8) & (df_dropped['Age']<=16),1,df_dropped['AgeStatus'] ) 
+#    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>16) & (df_dropped['Age']<=24),2,df_dropped['AgeStatus'] )
+#    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>24) & (df_dropped['Age']<=38),3,df_dropped['AgeStatus'] ) 
+#    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>38) & (df_dropped['Age']<=55),4,df_dropped['AgeStatus'] ) 
+#    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>55),5,df_dropped['AgeStatus'] ) 
+#    
+#    df_dropped['AgeStatus1'] = np.where(df_dropped['AgeStatus'] == 0, 1, 0)
+#    df_dropped['AgeStatus2'] = np.where(df_dropped['AgeStatus'] == 1, 1, 0)
+#    df_dropped['AgeStatus3'] = np.where(df_dropped['AgeStatus'] == 2, 1, 0)
+#    df_dropped['AgeStatus4'] = np.where(df_dropped['AgeStatus'] == 3, 1, 0)
+#    df_dropped['AgeStatus5'] = np.where(df_dropped['AgeStatus'] == 4, 1, 0)
+#    
+#    df_dropped = df_dropped.drop(columns = ['Age', 'AgeStatus'])
     
-    df_dropped['AgeStatus'] = np.nan
-    df_dropped['AgeStatus'] = np.where((df_dropped['Age']<=12),0,df_dropped['AgeStatus'])
-    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>12) & (df_dropped['Age']<=18),1,df_dropped['AgeStatus'] ) 
-    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>18) & (df_dropped['Age']<=24),2,df_dropped['AgeStatus'] )
-    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>24) & (df_dropped['Age']<=55),3,df_dropped['AgeStatus'] ) 
-    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>55) & (df_dropped['Age']<=70),4,df_dropped['AgeStatus'] ) 
-    df_dropped['AgeStatus'] = np.where((df_dropped['Age']>70),5,df_dropped['AgeStatus'] ) 
+
+    df_dropped['Title_mister'] = np.where(df_dropped['Title'] =='Mister', 1, 0)
+    df_dropped['Title_miss'] = np.where(df_dropped['Title'] =='Miss', 1, 0)
+    df_dropped['Title_madame'] = np.where(df_dropped['Title'] =='Madame', 1, 0)
+    df_dropped['Title_missus'] = np.where(df_dropped['Title'] =='Missus', 1, 0)
+    df_dropped['Title_master'] = np.where(df_dropped['Title'] =='Master', 1, 0)
+    df_dropped['Title_rare'] = np.where(df_dropped['Title'] =='Rare', 1, 0)
+    df_dropped['Title_madmo'] = np.where(df_dropped['Title'] =='Mademoiselle', 1, 0)
+    
+    df_dropped = df_dropped.drop(columns = 'Title')
+
+    df_dropped.loc[:,'IsAlone'] = np.where(df_dropped.loc[:,'Parch'] + df_dropped.loc[:,'SibSp'] == 0,1,0)
+    df_dropped.loc[:,'FamiliySize'] = df_dropped.loc[:,'Parch'] + df_dropped.loc[:,'SibSp'] + 1
+
+    df_dropped = df_dropped.drop(['Name', 'Parch','SibSp'], axis = 1)
+    
+    
+    label = LabelEncoder()
+    df_dropped['AgeBin'] = pd.qcut(df_dropped['Age'], 7)
+    df_dropped['AgeBin_Code'] = label.fit_transform(df_dropped['AgeBin'])
+    df_dropped.drop(['AgeBin','Age'], 1, inplace=True)
+    
+    df_dropped['Fare']=df_dropped['Fare'].fillna(df_dropped['Fare'].median())
+    df_dropped['FareBin'] = pd.qcut(df_dropped['Fare'], 5)
+    df_dropped['FareBin_Code'] = label.fit_transform(df_dropped['FareBin'])
+    df_dropped.drop(['Fare','FareBin'], 1, inplace=True)
 
 
-    df_dropped = df_dropped.drop(['Name','Age'], axis = 1)
-    df_drop_nonan = df_dropped.dropna()
+    df_dropped['isFemale'] = np.where(df_dropped['Sex'] =='female', 1, 0)
+    df_dropped['isMale'] = np.where(df_dropped['Sex'] =='male', 1, 0)
     
-    df_drop_nonan.loc[:,'IsAlone'] = np.where(df_drop_nonan.loc[:,'Parch'] + df_drop_nonan.loc[:,'SibSp'] == 0,1,0)
+    df_dropped = df_dropped.drop(columns = 'Sex')
+    
+    
+    df_dropped['Embarked_Q'] = np.where(df_dropped['Embarked'] =='Q', 1, 0)
+    df_dropped['Embarked_S'] = np.where(df_dropped['Embarked'] =='S', 1, 0)
+    df_dropped['Embarked_C'] = np.where(df_dropped['Embarked'] =='C', 1, 0)
+    
+    df_dropped = df_dropped.drop(columns = 'Embarked')
+    
+    
+    df_dropped['Pclass1'] = np.where(df_dropped['Pclass'] == 1, 1, 0)
+    df_dropped['Pclass2'] = np.where(df_dropped['Pclass'] == 2, 1, 0)
+    df_dropped['Pclass3'] = np.where(df_dropped['Pclass'] == 3, 1, 0)
+    
+    df_dropped = df_dropped.drop(columns = 'Pclass')
+
 
     
-    df_drop_nonan = df_drop_nonan.replace('male',1)
-    df_drop_nonan = df_drop_nonan.replace('female',0)
-    
-    df_drop_nonan['Embarked'] = df_drop_nonan['Embarked'].replace('S',0)
-    df_drop_nonan['Embarked'] = df_drop_nonan['Embarked'].replace('C',1)
-    df_drop_nonan['Embarked'] = df_drop_nonan['Embarked'].replace('Q',2)
-    
-    from scipy import stats
-    if drop_outliners == True:
-        df_drop_nonan = df_drop_nonan[(np.abs(stats.zscore(df_drop_nonan)) < 3).all(axis=1)]
-    
-    df_drop_nonan.reset_index(inplace = True)
-     
-    scaler = StandardScaler()
-    columns_to_scale = ['Fare']
-
-    temp_df = df_drop_nonan[columns_to_scale]
-
-    temp_df = pd.DataFrame(scaler.fit_transform(temp_df))
-    temp_df.columns = columns_to_scale
-    
-    df_drop_nonan_sc = df_drop_nonan.drop(columns_to_scale, axis = 1)
-    df_drop_nonan_sc = df_drop_nonan_sc.join(temp_df)
-    
-    return df_drop_nonan_sc
+    return df_dropped
--- titanic/main.py
+++ titanic/main.py
@@ -12,25 +12,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 
 import numpy as np
+np.random.seed()
+import random
+random.seed()
 
 from data_cleaning import clean_fe
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-test_cleaned = clean_fe(train)
+train_cleaned = clean_fe(train)
 
-X_train, X_test, y_train, y_test = train_test_split(test_cleaned.loc[:,'Pclass':].values,
-                                                    test_cleaned.loc[:,'Survived'].values,
-                                                    test_size=0.2)
+test_cleaned = clean_fe(test)
+
+X_train, X_test, y_train, y_test = train_test_split(train_cleaned.loc[:,'CabinA':].values,
+                                                    train_cleaned.loc[:,'Survived'].values,
+                                                    train_size=0.80)
 
 from sklearn.neighbors import KNeighborsClassifier
 
-knc = KNeighborsClassifier(n_jobs = 1)
+knc = KNeighborsClassifier()
 
-parameters = {'n_neighbors': np.arange(2,6)}
+parameters = {'n_neighbors': np.arange(2,9)}
 
-knc = GridSearchCV(knc, parameters, cv=5)
+knc = GridSearchCV(knc, parameters, cv=5, iid = False)
 
 knc.fit(X_train, y_train)
 
@@ -45,10 +50,10 @@ from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier()
 
 #create a dictionary of all values we want to test for n_estimators
-params_rf = {'n_estimators': np.arange(20, 30) }
+params_rf = {'n_estimators': np.arange(15, 45)}
 
 #use gridsearch to test all values for n_estimators
-rf_gs = GridSearchCV(rf, params_rf, cv=5)
+rf_gs = GridSearchCV(rf, params_rf, cv=5, iid = False)
 
 #fit model to training data
 rf_gs.fit(X_train, y_train)
@@ -66,10 +71,10 @@ from sklearn.svm import SVC
 svc = SVC(gamma = 'scale', probability=True)
 
 #create a dictionary of all values we want to test for n_estimators
-params_svc = {'C' : np.arange(1,4)}
+params_svc = {'C' : np.arange(0.1,10,0.5)}
 
 #use gridsearch to test all values for n_estimators
-svc_gs = GridSearchCV(svc, params_svc, cv=5)
+svc_gs = GridSearchCV(svc, params_svc, cv=5, iid = False)
 
 #fit model to training data
 svc_gs.fit(X_train, y_train)
@@ -87,10 +92,10 @@ from sklearn.ensemble import GradientBoostingClassifier
 gbc = GradientBoostingClassifier()
 
 #create a dictionary of all values we want to test for n_estimators
-params_gbc = {'n_estimators': np.arange(25,45,1),'learning_rate': np.arange(0.01,0.4,0.01) }
+params_gbc = {'n_estimators': np.arange(25,45,1) }
 
 #use gridsearch to test all values for n_estimators
-gbc_gs = GridSearchCV(gbc, params_gbc, cv=5)
+gbc_gs = GridSearchCV(gbc, params_gbc, cv=5, iid = False)
 
 #fit model to training data
 gbc_gs.fit(X_train, y_train)
@@ -104,11 +109,11 @@ print('gbc: ',gbc_gs.best_params_, gbc_gs.best_score_)
 
 import xgboost as xgb
 
-params_xgb = {'max_depth': np.arange(3,7)}
+params_xgb = {'max_depth': np.arange(2,10)}
 
 xgb_model = xgb.XGBClassifier()
 
-xgb_gs = GridSearchCV(xgb_model, params_xgb, cv=5)
+xgb_gs = GridSearchCV(xgb_model, params_xgb, cv=5, iid = False)
 
 xgb_gs.fit(X_train, y_train)
 
@@ -123,10 +128,10 @@ from sklearn.ensemble import AdaBoostClassifier
 adc = AdaBoostClassifier()
 
 #create a dictionary of all values we want to test for n_estimators
-params_adc = {'n_estimators': np.arange(22,34,2), 'learning_rate': np.arange(0.01,1.0,0.051) }
+params_adc = {'n_estimators': np.arange(22,34,2), 'learning_rate': np.arange(0.01,1.0,0.01) }
 
 #use gridsearch to test all values for n_estimators
-adc_gs = GridSearchCV(adc, params_adc, cv=5)
+adc_gs = GridSearchCV(adc, params_adc, cv=5, iid = False)
 
 #fit model to training data
 adc_gs.fit(X_train, y_train)
@@ -141,7 +146,7 @@ print('adc: ',adc_gs.best_params_, adc_gs.best_score_)
 from sklearn.linear_model import LogisticRegression
 
 #create a new logistic regression model
-log_reg = LogisticRegression(solver = 'saga',tol = 1e-6, max_iter = 400, penalty = 'elasticnet', l1_ratio = 0.5)
+log_reg = LogisticRegression(solver = 'saga',tol = 1e-6, max_iter = 2000, penalty = 'elasticnet', l1_ratio = 0.5)
 
 #fit the model to the training data
 log_reg.fit(X_train, y_train)
@@ -149,11 +154,33 @@ log_reg.fit(X_train, y_train)
 print('log_reg', log_reg.score(X_train,y_train))
 
 
+from sklearn.ensemble import ExtraTreesClassifier
 
+#create a new random forest classifier
+etc = ExtraTreesClassifier()
+
+#create a dictionary of all values we want to test for n_estimators
+params_etc = {'n_estimators': np.arange(80,150,5)}
+
+#use gridsearch to test all values for n_estimators
+etc_gs = GridSearchCV(etc, params_etc, cv=5, iid = False)
+
+#fit model to training data
+etc_gs.fit(X_train, y_train)
+
+#save best model
+etc_best = etc_gs.best_estimator_
+
+#check best n_estimators value
+print('etc: ',etc_gs.best_params_, etc_gs.best_score_)
+
+
+
+#Voting classifier
 from sklearn.ensemble import VotingClassifier
 
 #create a dictionary of our models
-estimators=[('knc',knc_best), ('rf', rf_best), ('log_reg', log_reg), 
+estimators=[('rf', rf_best),('knc',knc_best), ('log_reg', log_reg), ('etc',etc_best),
             ('gbc', gbc_best), ('SVC',svc_best),('ADC',adc_best),('xgb',xgb_best)]
 #create our voting classifier, inputting our models
 ensemble = VotingClassifier(estimators, voting='soft')
@@ -166,12 +193,8 @@ print(ensemble.score(X_test, y_test))
 
 
 
-#Create test data. Make a function next time, wtf.
-
-test_cleaned = clean_fe(test, False)
-
 submission = pd.DataFrame()
-prediction = pd.DataFrame(ensemble.predict(test_cleaned.loc[:,'Pclass':].values))
+prediction = pd.DataFrame(ensemble.predict(test_cleaned.loc[:,'CabinA':].values))
 
 submission['Survived']  = prediction[0]
 submission['PassengerId']=test['PassengerId']
